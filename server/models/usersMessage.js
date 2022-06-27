822e392745ab3bd338bb90d65ec4fcbb1f9d52ba
BG <mavie1322@yahoo.fr>
2022-06-27 12:26:40 +0100

    create user schema

---
+++ server/models/usersMessage.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  email: { type: String, required: true, unique: true },
+  password: String,
+  address: {
+    street_address: String,
+    flat_number: String,
+    city: String,
+    postcode: String,
+    country: String,
+  },
+  delivery_address: {
+    type: Array,
+    default: [],
+  },
+  basket: {
+    type: Array,
+    default: [],
+  },
+  orders: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+    ref: "orders",
+  },
+});
+
+const UsersMessage = mongoose.model("users", userSchema);
+
+export default UsersMessage;
