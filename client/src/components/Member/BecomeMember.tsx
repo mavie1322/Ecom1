591a90f463115f4a63122f8d6461fad88ab16462
BG <mavie1322@yahoo.fr>
2022-06-27 09:49:16 +0100

    simplify the compenent so we can modify it later for the no relational database I am building

--- client/src/components/Member/BecomeMember.tsx
+++ client/src/components/Member/BecomeMember.tsx
@@ -6,50 +6,51 @@ interface Props {
 
 const BecomeMember: React.FC<Props> = ({ togglePopup }) => {
   return (
-    <div className='popup-box'>
-      <div className='box scale-up-ver-bottom'>
-        <span className='close-icon' onClick={() => togglePopup()}>
-          X
-        </span>
-        {
-          <div className='sign-in'>
-            <header className='sign-in__header font-styling'>Sign in</header>
-            <form>
-              <div className='sign-in__inputs'>
-                <span>
-                  <label>Email</label>
-                  <p>*</p>
-                </span>
-                <input type='email' required />
-              </div>
-              <div className='sign-in__inputs'>
-                <span>
-                  <label>Password</label>
-                  <p>*</p>
-                </span>
-                <input type='email' required />
-              </div>
-              <div className='sign-in__option'>
-                <span>Forgot password?</span>
-              </div>
-              <div className='sign-in__buttons'>
-                <button type='submit' className='sign-in__button font-styling'>
-                  Sign In
-                </button>
-              </div>
-              <div className='sign-in__buttons'>
-                <button
-                  type='submit'
-                  className='sign-in__become-member font-styling'
-                  onClick={() => togglePopup()}>
-                  Become a member
-                </button>
-              </div>
-            </form>
-          </div>
-        }
-      </div>
-    </div>
+    <div>Become Member</div>
+    // <div className='popup-box'>
+    //   <div className='box scale-up-ver-bottom'>
+    //     <span className='close-icon' onClick={() => togglePopup()}>
+    //       X
+    //     </span>
+    //     {
+    //       <div className='sign-in'>
+    //         <header className='sign-in__header font-styling'>Sign in</header>
+    //         <form>
+    //           <div className='sign-in__inputs'>
+    //             <span>
+    //               <label>Email</label>
+    //               <p>*</p>
+    //             </span>
+    //             <input type='email' required />
+    //           </div>
+    //           <div className='sign-in__inputs'>
+    //             <span>
+    //               <label>Password</label>
+    //               <p>*</p>
+    //             </span>
+    //             <input type='email' required />
+    //           </div>
+    //           <div className='sign-in__option'>
+    //             <span>Forgot password?</span>
+    //           </div>
+    //           <div className='sign-in__buttons'>
+    //             <button type='submit' className='sign-in__button font-styling'>
+    //               Sign In
+    //             </button>
+    //           </div>
+    //           <div className='sign-in__buttons'>
+    //             <button
+    //               type='submit'
+    //               className='sign-in__become-member font-styling'
+    //               onClick={() => togglePopup()}>
+    //               Become a member
+    //             </button>
+    //           </div>
+    //         </form>
+    //       </div>
+    //     }
+    //   </div>
+    // </div>
   );
 };
 
