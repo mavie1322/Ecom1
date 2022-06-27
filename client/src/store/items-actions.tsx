3b3149fe317b7f392e32fef699e03f6fdb5a3daf
BG <mavie1322@yahoo.fr>
2022-06-27 11:40:44 +0100

    change the interface so it would match informations fetched from the database

--- client/src/store/items-actions.tsx
+++ client/src/store/items-actions.tsx
@@ -1,11 +1,11 @@
-import { ArticleFromApi } from "../models";
+import { Article } from "../models";
 import { getItems } from "../services/api";
 import { itemsActions } from "./items-slice";
 
 export const fetchItems = () => {
   return async (dispatch: any) => {
     try {
-      const itemsList: ArticleFromApi = await getItems();
+      const itemsList: Article[] = await getItems();
       dispatch(itemsActions.storeItems(itemsList));
     } catch (err: any) {
       throw new Error(err);
