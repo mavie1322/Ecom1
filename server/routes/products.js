ae7c355eaadcb71368b8851970393e39bb2c3894
BG <mavie1322@yahoo.fr>
2022-06-27 12:25:11 +0100

    add a router to get single product informations

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getProducts } from "../controllers/products.js";
+import { getProducts, getSingleProduct } from "../controllers/products.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
+router.get("/:id", getSingleProduct);
 
 export default router;
