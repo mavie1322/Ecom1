a5ea92fc8a93e061ef7f83deb7a9861ed4d52ccc
BG <mavie1322@yahoo.fr>
2022-06-27 12:23:41 +0100

    create users routers to sign in and up

---
+++ server/routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { signInUser, signUpUser } from "../controllers/users.js";
+
+const router = express.Router();
+
+router.post("/signin/", signInUser);
+router.post("/signup", signUpUser);
+
+export default router;
