8661a75cf5301708cd217450cb0a033a90e649eb
BG <mavie1322@yahoo.fr>
2022-06-27 10:57:43 +0100

    change the type of the variable

--- client/src/store/categories-actions.tsx
+++ client/src/store/categories-actions.tsx
@@ -1,11 +1,11 @@
-import { CategoriesState } from "../models";
+import { Category } from "../models";
 import { getCategories } from "../services/api";
 import { categoriesActions } from "./categories-slices";
 
 export const fetchCategories = () => {
   return async (dispatch: any) => {
     try {
-      const categoriesList: CategoriesState = await getCategories();
+      const categoriesList: Category[] = await getCategories();
       dispatch(categoriesActions.storeCategories(categoriesList));
     } catch (err: any) {
       throw new Error(err);
