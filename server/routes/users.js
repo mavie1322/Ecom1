e5e59737e5285b388adf0e1dd6e588eed74a2406
BG <mavie1322@yahoo.fr>
2022-07-01 10:58:28 +0100

    add new routes to modify data in the user models

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { signInUser, signUpUser } from "../controllers/users.js";
+import {
+  signInUser,
+  signUpUser,
+  addToBasket,
+  deleteProduct,
+  updateBasket,
+} from "../controllers/users.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/signin/", signInUser);
 router.post("/signup", signUpUser);
+
+router.post("/:id/basket", auth, addToBasket);
+router.patch("/:id/basket", auth, updateBasket);
 
 export default router;
