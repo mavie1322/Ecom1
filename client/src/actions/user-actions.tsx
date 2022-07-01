18fb0d9f241c0d1b8f082985ab71fd88c1681087
BG <mavie1322@yahoo.fr>
2022-07-01 09:35:54 +0100

    add basket property so it is reset when user sign in and action added to delete items or modify quantity in the cart

--- client/src/actions/user-actions.tsx
+++ client/src/actions/user-actions.tsx
@@ -1,5 +1,10 @@
-import { UserDetails, User } from "../models";
-import { createUser, logInUser } from "../services/api";
+import { UserDetails, User, BasketItem } from "../models";
+import {
+  addItemsToBasket,
+  createUser,
+  deleteItemInBasket,
+  logInUser,
+} from "../services/api";
 import { errorsActions } from "../store/errors-slices";
 import { userActions } from "../store/user-slices";
 
@@ -11,6 +16,7 @@ export const signIn = (
   return async (dispatch: any) => {
     try {
       const userProfile: User = await logInUser(userDetails);
+      console.log(userProfile);
       dispatch(userActions.storeUser(userProfile));
       togglePopup();
       setUserDetails({
@@ -18,6 +24,7 @@ export const signIn = (
         last_name: "",
         email: "",
         password: "",
+        basket: [],
       });
       dispatch(errorsActions.errorUserLoggedIn(false));
     } catch (err: any) {
@@ -34,6 +41,46 @@ export const signUp = (userDetails: UserDetails) => {
     } catch (error: any) {
       if (error.response.data.message === "User already exists.")
         dispatch(errorsActions.errorUserCreation("false"));
+    }
+  };
+};
+
+export const addToBasketApi = (items: BasketItem[], id: string) => {
+  return async (dispatch: any) => {
+    try {
+      const newUserProfile: User = await addItemsToBasket(items, id);
+      dispatch(userActions.updateBasket(newUserProfile));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const deleteItemApi = (itemId: string, userId: string) => {
+  return async (dispatch: any) => {
+    try {
+      const newUserProfile: User = await deleteItemInBasket(
+        { id: itemId },
+        userId
+      );
+      console.log(newUserProfile);
+      dispatch(userActions.storeUser(newUserProfile));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const changeItemQuantity = (
+  itemQuantity: [number, string],
+  userId: string
+) => {
+  return async (dispatch: any) => {
+    try {
+      const newUserProfile = await changeItemQuantity(itemQuantity, userId);
+      console.log(newUserProfile);
+    } catch (error) {
+      console.log(error);
     }
   };
 };
