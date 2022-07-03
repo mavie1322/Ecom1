af9f4a6efce1c23359adb4c76c10171650a5c959
BG <mavie1322@yahoo.fr>
2022-07-03 22:47:42 +0100

    clean up the code

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,11 +4,8 @@ export const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     let decodedData = jwt.verify(token, process.env.SECRET);
-    console.log(decodedData, "auth");
     req.email = decodedData.email;
     req.userId = decodedData.id;
-    // console.log(typeof req.userId);
-    // console.log(req.email);
     next();
   } catch (error) {
     console.log(error);
