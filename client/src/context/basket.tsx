0186dd0f81ff58dfe0a827fcbd9ad5e8fe315523
BG <mavie1322@yahoo.fr>
2022-07-07 12:14:57 +0100

    add a new state thath define if user is on the checkout page and will help to define which elements will be visible

--- client/src/context/basket.tsx
+++ client/src/context/basket.tsx
@@ -5,6 +5,11 @@ export const BasketContext = createContext<BasketContextType | null>(null);
 
 export const BasketProvider = ({ children }: { children: any }) => {
   const [itemsInBasket, setItemsInBasket] = useState<BasketItem[]>([]);
+  const [isCheckout, setCheckout] = useState<boolean>(false);
+
+  const changeCheckout = () => {
+    setCheckout(!isCheckout);
+  };
 
   const addToBasket = (product: BasketItem) => {
     let isItemExist = itemsInBasket.findIndex(
@@ -29,7 +34,8 @@ export const BasketProvider = ({ children }: { children: any }) => {
   };
 
   return (
-    <BasketContext.Provider value={{ itemsInBasket, addToBasket, clear }}>
+    <BasketContext.Provider
+      value={{ itemsInBasket, addToBasket, clear, isCheckout, changeCheckout }}>
       {children}
     </BasketContext.Provider>
   );
