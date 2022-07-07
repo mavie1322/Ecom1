717b4d7511f4631c67fb74838b582eaeca3646fa
BG <mavie1322@yahoo.fr>
2022-07-07 13:32:50 +0100

    model an input component useful for form

---
+++ client/src/containers/Input.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+type Props = {
+  label: string;
+  isRequired: boolean;
+  name: string;
+  type: string;
+  value: string;
+  change: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  focus: boolean;
+};
+
+const Input: React.FC<Props> = ({
+  label,
+  isRequired,
+  name,
+  type,
+  value,
+  change,
+  focus,
+}) => {
+  return (
+    <div className='sign-in__inputs'>
+      <span>
+        <label>{label}</label>
+        <p>{isRequired ? "*" : ""}</p>
+      </span>
+      <input
+        required={isRequired}
+        name={name}
+        type={type}
+        value={value}
+        onChange={change}
+        style={{ backgroundColor: "white" }}
+        autoFocus={focus}
+      />
+    </div>
+  );
+};
+
+export default Input;
