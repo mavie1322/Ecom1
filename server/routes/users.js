3411337df918f75cb667318939c1c5d0dbfe8758
BG <mavie1322@yahoo.fr>
2022-07-09 00:45:47 +0100

    remove double authentification of users

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,12 +6,13 @@ import {
   editAddresses,
 } from "../controllers/users.js";
 import { auth } from "../middleware/auth.js";
+import Stripe from "stripe";
 
 const router = express.Router();
 
 router.post("/signin/", signInUser);
 router.post("/signup", signUpUser);
 router.patch("/:id/basket", auth, updateBasket);
-router.patch("/:id/checkout", auth, auth, editAddresses);
+router.patch("/:id/checkout", auth, editAddresses);
 
 export default router;
