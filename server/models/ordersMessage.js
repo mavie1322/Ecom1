bf9cbebd491be0d4ad89dd2558b54838424ed96d
BG <mavie1322@yahoo.fr>
2022-07-09 00:40:30 +0100

    change the structure of the order schema according information retrieve on paymnent on stripe

--- server/models/ordersMessage.js
+++ server/models/ordersMessage.js
@@ -1,16 +1,28 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema({
-  items: [
+  products: [
     {
-      item_basket: { type: mongoose.Schema.Types.ObjectId, ref: "products" },
-      quantity_ordered: { type: Number },
+      item: {
+        type: String,
+      },
+      qty: { type: Number },
     },
   ],
-  total_cost: Number,
+
   createdAt: { type: Date, default: new Date() },
-  buyer: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  delivery_cost: Boolean,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  paymentIntentId: { type: String },
+  stripeCustomerId: { type: String },
+  subtotal: { type: Number, required: true },
+  total: { type: Number, required: true },
+  shipping: { type: Object, required: true },
+  delivery_status: { type: String, default: "pending" },
+  payment_status: { type: String, required: true },
 });
 
 const OrdersMessage = mongoose.model("orders", orderSchema);
