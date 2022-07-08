260f5dd8016eec7d5c2f2275aa833cc096cb14e8
BG <mavie1322@yahoo.fr>
2022-07-09 00:39:05 +0100

    create endpoint route so user can pay with stripe

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+// import Stripe from "stripe";
 
 import ProductsRoutes from "./routes/products.js";
 import CategoriesRoutes from "./routes/categories.js";
 import UsersRoutes from "./routes/users.js";
+import StripeRoutes from "./routes/stripes.js";
 
 const app = express();
 dotenv.config();
@@ -18,6 +20,7 @@ app.use(cors());
 app.use("/products", ProductsRoutes);
 app.use("/categories", CategoriesRoutes);
 app.use("/users", UsersRoutes);
+app.use("/stripe", StripeRoutes);
 
 mongoose
   .connect(process.env.CONNECTION_URL, {
