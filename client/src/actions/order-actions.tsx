2e623a6a730b19a272d001195199e6ad89728c62
BG <mavie1322@yahoo.fr>
2022-07-09 00:29:00 +0100

    store function to create and pull orders

---
+++ client/src/actions/order-actions.tsx
@@ -0,0 +1,14 @@
+import { BasketItem } from "../models";
+import { payWithStripe } from "../services/api";
+
+export const payOrder = (basketItems: BasketItem[], userId: string) => {
+  return async (dispatch: any) => {
+    try {
+      const order = await payWithStripe({ basketItems, userId });
+      if (order.url) window.location.href = order.url;
+      console.log(order);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
