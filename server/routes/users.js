7f0081ab98133e34c29e2883d4f6918000224835
BG <mavie1322@yahoo.fr>
2022-07-16 21:55:20 +0100

    add the endpont to get user details

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,15 +4,16 @@ import {
   signUpUser,
   updateBasket,
   editAddresses,
+  getUserById,
 } from "../controllers/users.js";
 import { auth } from "../middleware/auth.js";
-import Stripe from "stripe";
 
 const router = express.Router();
 
-router.post("/signin/", signInUser);
+router.post("/signin", signInUser);
 router.post("/signup", signUpUser);
 router.patch("/:id/basket", auth, updateBasket);
 router.patch("/:id/checkout", auth, editAddresses);
+router.get("/:id/", getUserById);
 
 export default router;
