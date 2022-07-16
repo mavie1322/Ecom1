938362663b945bedc4768391dda409efdb4e120f
BG <mavie1322@yahoo.fr>
2022-07-16 20:26:29 +0100

    add the jsx elements to the component

--- client/src/components/Checkout/CheckoutSuccessful/SucceedCheckout.tsx
+++ client/src/components/Checkout/CheckoutSuccessful/SucceedCheckout.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Params, useNavigate, useParams } from "react-router-dom";
+import { getUser } from "../../../actions/user-actions";
+import { BasketContext } from "../../../context/basket";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import { BasketContextType } from "../../../models";
 import "./succeedCheckout.css";
 
 const SucceedCheckout = () => {
-  return <div>Succeed Checkout</div>;
+  const id: Readonly<Params<string>> = useParams();
+  const { isCheckout, changeCheckout } = useContext(
+    BasketContext
+  ) as BasketContextType;
+  const user = useAppSelector((state) => state.user.result);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  console.log("checkout >>>>>", isCheckout);
+  console.log("user >>>>", user);
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
+  useEffect(() => {
+    if (isCheckout === false) {
+      changeCheckout();
+    }
+
+    dispatch(getUser(id));
+  }, [changeCheckout, dispatch, id, isCheckout]);
+
+  return (
+    <div className='succeedCheckout section__margin'>
+      <h1>Thank you for your purchase!</h1>
+      <span>
+        <p>
+          Your order number is :{" "}
+          <strong>{user.orders[user.orders.length - 1]}</strong>
+        </p>
+        <p>We'll email you an order confirmation with details</p>
+      </span>
+      <button
+        type='button'
+        className='basketMenu__checkout font-styling'
+        onClick={() => handleClick()}>
+        Continue Shopping
+      </button>
+    </div>
+  );
 };
 
 export default SucceedCheckout;
