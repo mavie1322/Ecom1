235c72cae4d5c888b8edbd9f66631038b07161ff
BG <mavie1322@yahoo.fr>
2022-07-18 18:11:48 +0100

    get the orders list from the database and keep it the redux store

--- client/src/actions/order-actions.tsx
+++ client/src/actions/order-actions.tsx
@@ -1,11 +1,24 @@
 import { BasketItem } from "../models";
-import { payWithStripe } from "../services/api";
+import { fetchOrdersByUser, payWithStripe } from "../services/api";
+import { ordersActions } from "../store/orders-slices";
 
 export const payOrder = (basketItems: BasketItem[], userId: string) => {
   return async (dispatch: any) => {
     try {
       const order = await payWithStripe({ basketItems, userId });
       if (order.url) window.location.href = order.url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const getOrdersById = (orders: string[]) => {
+  return async (dispatch: any) => {
+    try {
+      const ordersApi = await fetchOrdersByUser({ orders });
+      dispatch(ordersActions.storeOrders(ordersApi.orders));
+      console.log(ordersApi);
     } catch (error) {
       console.log(error);
     }
