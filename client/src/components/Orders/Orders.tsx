e771e1bff99f2b4c637c511ed9319e4b354a13c1
BG <mavie1322@yahoo.fr>
2022-07-18 19:13:19 +0100

    check if the orders list fetched from api has successfully been stored and accessible

--- client/src/components/Orders/Orders.tsx
+++ client/src/components/Orders/Orders.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-import { useAppSelector } from "../../hooks/hooks";
+import React, { useEffect } from "react";
+import { getOrdersById } from "../../actions/order-actions";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import "./orders.css";
 
 const Orders = () => {
-  const orders = useAppSelector((state) => state.user);
+  const orders = useAppSelector((state) => state.user.result.orders);
+  const user = useAppSelector((state) => state.orders);
+
+  const dispatch = useAppDispatch();
+  console.log("order >>>", orders);
+  console.log("user >>>", user);
+
+  useEffect(() => {
+    dispatch(getOrdersById(orders));
+  }, [orders, dispatch]);
   console.log(orders);
   return (
     <div>
