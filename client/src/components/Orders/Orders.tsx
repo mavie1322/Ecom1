ae822feb94a548ec3451680cd4edeaeb193f4b23
BG <mavie1322@yahoo.fr>
2022-07-25 13:57:35 +0100

    add code block so list of orders will be displayed

--- client/src/components/Orders/Orders.tsx
+++ client/src/components/Orders/Orders.tsx
@@ -1,23 +1,41 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { getOrdersById } from "../../actions/order-actions";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { AiOutlineRight } from "react-icons/ai";
 import "./orders.css";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
-  const orders = useAppSelector((state) => state.user.result.orders);
-  const user = useAppSelector((state) => state.orders);
-
+  const orders = useAppSelector((state) => state.orders.purchases);
+  const user = useAppSelector((state) => state.user.result);
   const dispatch = useAppDispatch();
-  console.log("order >>>", orders);
-  console.log("user >>>", user);
 
   useEffect(() => {
-    dispatch(getOrdersById(orders));
-  }, [orders, dispatch]);
-  console.log(orders);
+    dispatch(getOrdersById(user.orders));
+  }, [user, dispatch]);
+
   return (
-    <div>
+    <div className='order__container'>
       <h1>My Purchases</h1>
+      <section className='order__section'>
+        {orders.map((order) => {
+          const dateArray = order.createdAt
+            .substring(0, 10)
+            .replaceAll("-", "/")
+            .split("/")
+            .reverse();
+          const date = dateArray.join("/");
+          return (
+            <Link
+              key={order._id}
+              className='order__span link'
+              to={`/users/${user._id}/orders/${order._id}`}>
+              <h1>{date}</h1>
+              <AiOutlineRight size={22} className='order__icon' />
+            </Link>
+          );
+        })}
+      </section>
     </div>
   );
 };
