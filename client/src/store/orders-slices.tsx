10b790e50ad9f2537af909bc3ffbe8d5ea5ee6af
BG <mavie1322@yahoo.fr>
2022-07-25 14:30:26 +0100

    change the initial stateof order slices

--- client/src/store/orders-slices.tsx
+++ client/src/store/orders-slices.tsx
@@ -1,34 +1,16 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Orders } from "../models";
 
-const initialOrdersState: Orders = {
-  _id: "",
-  products: [],
-  subtotal: 0,
-  total: 0,
-  createdAt: "",
-  userId: "",
-  delivery_status: "",
-  payment_status: "",
-  shipping: {
-    first_name: "",
-    last_name: "",
-    street_address: "",
-    flat_number: "",
-    city: "",
-    postcode: "",
-    country: "",
-  },
-  stripeCustomerId: "",
-  __v: 0,
+const initialOrdersState: { purchases: Orders[] } = {
+  purchases: [],
 };
 
 export const ordersSlice = createSlice({
   name: "orders",
   initialState: initialOrdersState,
   reducers: {
-    storeOrders(state, action: PayloadAction<Orders>) {
-      state = action.payload;
+    storeOrders(state, action: PayloadAction<Orders[]>) {
+      state.purchases = action.payload;
     },
   },
 });
