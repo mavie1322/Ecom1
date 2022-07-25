609543a409633b330acd115e2a153d745da0355c
BG <mavie1322@yahoo.fr>
2022-07-25 14:59:23 +0100

    reserve the array so the recent order will be on the top

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,11 +1,12 @@
 import OrdersMessage from "../models/ordersMessage.js";
 
 export const getOrders = async (req, res) => {
-  //   console.log(">>>", req.body.orders);
+  console.log(">>>", req.body.orders);
   const orders = req.body.orders;
   try {
     const result = await OrdersMessage.find({ _id: { $in: orders } });
-    res.status(200).send({ orders: result });
+    const reversedOrders = result.reverse();
+    res.status(200).send({ orders: reversedOrders });
   } catch (error) {
     console.log(error);
   }
