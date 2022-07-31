d17559fd823a9534e23230269eedce66c560e27f
BG <mavie1322@yahoo.fr>
2022-08-01 00:07:18 +0100

    add the block code needed to display the total cost and billing address

--- client/src/components/SingleOrder/SingleOrder.tsx
+++ client/src/components/SingleOrder/SingleOrder.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ProfileAccount from "../../containers/ProfileAccount";
 import { useAppSelector } from "../../hooks/hooks";
 import { BasketItem } from "../../models";
@@ -8,6 +8,7 @@ import "./singleOrder.css";
 const SingleOrder = () => {
   const { id } = useParams();
   const items = useAppSelector((state) => state.items.itemsList);
+  const user = useAppSelector((state) => state.user.result);
   const orders = useAppSelector((state) => state.orders.purchases);
   const order = orders.find((singleOrder) => singleOrder._id === id);
 
@@ -46,12 +47,14 @@ const SingleOrder = () => {
                 );
                 return (
                   <div className='singleOrder__main-container'>
-                    <img
-                      src={productDetails?.img_url}
-                      alt={productDetails?.item_name}
-                    />
-                    <div>
-                      <span className='singleOrder__main-header'>
+                    <Link to={`/products/${productDetails?._id}`}>
+                      <img
+                        src={productDetails?.img_url}
+                        alt={productDetails?.item_name}
+                      />
+                    </Link>
+                    <div className='flex-start'>
+                      <span className='singleOrder__main-header flex-start'>
                         <p>{productDetails?.item_name}</p>
                         <p>£{(productDetails!.price / 100).toFixed(2)}</p>
                       </span>
@@ -80,23 +83,54 @@ const SingleOrder = () => {
                 );
               })}
             </div>
-            <div>
-              <span>
-                <h4>Delivery address</h4>
-                <span>
-                  <p>{`${order?.shipping.first_name} ${order?.shipping.flat_number}`}</p>
-                  <p>{order?.shipping.street_address}</p>
-                  <p>{order?.shipping.flat_number}</p>
-                  <p>{`${order?.shipping.postcode} ${order?.shipping.city}`}</p>
-                  <p>{order?.shipping.country}</p>
-                </span>
-                <h4>Payment method</h4>
+            <div className='singleOrder__details flex-start'>
+              <h4>Delivery address</h4>
+              <div className='flex-start'>
+                <p>{`${order?.shipping.first_name} ${order?.shipping.flat_number}`}</p>
+                <p>{order?.shipping.street_address}</p>
+                <p>{order?.shipping.flat_number}</p>
+                <p>{`${order?.shipping.postcode} ${order?.shipping.city}`}</p>
+                <p>{order?.shipping.country}</p>
+              </div>
+              <h4>Payment method</h4>
+              <div>
                 <p>VISA</p>
                 <p>**** **** **** 4242</p>
-              </span>
+              </div>
             </div>
           </section>
-          <section></section>
+          <section className='singleOrder__cost'>
+            <span>
+              <p>Total:</p>
+              <p>£{(order!.subtotal / 100).toFixed(2)}</p>
+            </span>
+            <span>
+              <p>Delivery:</p>
+              <p>£3.99</p>
+            </span>
+            <span>
+              <p>Total:</p>
+              <p>£{(order!.total / 100).toFixed(2)}</p>
+            </span>
+          </section>
+          <section className='singleOrder__userDetails flex-start'>
+            <span className='flex-start'>
+              <h4>My Details</h4>
+              <div className='flex-start'>
+                <p>{`${user.first_name} ${user.last_name}`}</p>
+                <p>{user.email}</p>
+              </div>
+            </span>
+            <span className='flex-start'>
+              <h4>Billing Address</h4>
+              <div className='flex-start'>
+                <p>{user.address.street_address}</p>
+                <p>{user.address.flat_number}</p>
+                <p>{`${user.address.postcode} ${user.address.city}`}</p>
+                <p>{user.address.country}</p>
+              </div>
+            </span>
+          </section>
         </main>
       </div>
     </div>
