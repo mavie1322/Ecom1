5742faab6c6f917a402827b88869cdf09d545a8f
BG <mavie1322@yahoo.fr>
2022-08-04 02:00:55 +0100

    clean up the code

--- server/controllers/orders.js
+++ server/controllers/orders.js
@@ -1,7 +1,6 @@
 import OrdersMessage from "../models/ordersMessage.js";
 
 export const getOrders = async (req, res) => {
-  console.log(">>>", req.body.orders);
   const orders = req.body.orders;
   try {
     const result = await OrdersMessage.find({ _id: { $in: orders } });
