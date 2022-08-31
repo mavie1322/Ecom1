d6bd285d9cddb4ef57b784c751bf7b9763a18c9e
BG <mavie1322@yahoo.fr>
2022-08-31 18:26:31 +0100

    display message when there is no order

--- client/src/components/Orders/Orders.tsx
+++ client/src/components/Orders/Orders.tsx
@@ -3,12 +3,13 @@ import { getOrdersById } from "../../actions/order-actions";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { AiOutlineRight } from "react-icons/ai";
 import "./orders.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Orders = () => {
   const orders = useAppSelector((state) => state.orders.purchases);
   const user = useAppSelector((state) => state.user.result);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getOrdersById(user.orders));
@@ -18,23 +19,33 @@ const Orders = () => {
     <div className='order__container'>
       <h1>My Purchases</h1>
       <section className='order__section'>
-        {orders.map((order) => {
-          const dateArray = order.createdAt
-            .substring(0, 10)
-            .replaceAll("-", "/")
-            .split("/")
-            .reverse();
-          const date = dateArray.join("/");
-          return (
-            <Link
-              key={order._id}
-              className='order__span link'
-              to={`/users/${user._id}/orders/${order._id}`}>
-              <h2>{date}</h2>
-              <AiOutlineRight size={22} className='order__icon' />
-            </Link>
-          );
-        })}
+        {orders.length > 0 ? (
+          orders.map((order) => {
+            const dateArray = order.createdAt
+              .substring(0, 10)
+              .replaceAll("-", "/")
+              .split("/")
+              .reverse();
+            const date = dateArray.join("/");
+            return (
+              <Link
+                key={order._id}
+                className='order__span link'
+                to={`/users/${user._id}/orders/${order._id}`}>
+                <h2>{date}</h2>
+                <AiOutlineRight size={22} className='order__icon' />
+              </Link>
+            );
+          })
+        ) : (
+          <div className='order__div'>
+            <h4>No order</h4>
+            <p>When you have bought something online, you'll find it here.</p>
+            <button type='button' onClick={() => navigate("/")}>
+              Continue Shopping
+            </button>
+          </div>
+        )}
       </section>
     </div>
   );
