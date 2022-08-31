211a9ab494f956524c8b28f87223e1d4bf3be938
BG <mavie1322@yahoo.fr>
2022-08-31 18:52:26 +0100

    add onClick event so user can sign out

--- client/src/components/SignIn/Submenu/SignInSubmenu.tsx
+++ client/src/components/SignIn/Submenu/SignInSubmenu.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./signInSubmenu.css";
 
-import { Link } from "react-router-dom";
-import { useAppSelector } from "../../../hooks/hooks";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import { userActions } from "../../../store/user-slices";
 
 const SignInSubmenu = () => {
   const user = useAppSelector((state) => state.user.result);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleSignOut = () => {
+    dispatch(userActions.resetUser());
+    navigate("/");
+  };
+
   return (
     <div className='sign-submenu_container'>
       <Link to={`/users/${user._id}`} className='link sign-submenu_link'>
         My Account
       </Link>
-      <p>Sign out</p>
+      <p onClick={() => handleSignOut()}>Sign out</p>
     </div>
   );
 };
