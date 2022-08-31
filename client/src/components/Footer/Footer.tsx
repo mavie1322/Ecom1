98f3853f24b143b952f96c562af680568a8ed81d
BG <mavie1322@yahoo.fr>
2022-08-31 18:22:36 +0100

    design roughly the component. I need to add logo for all mediasand link p elements to different sections of the application

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,8 +1,51 @@
 import React from "react";
+import { useAppSelector } from "../../hooks/hooks";
 import "./footer.css";
 
 const Footer = () => {
-  return <div>footer</div>;
+  const categories = useAppSelector((state) => state.categories);
+  return (
+    <footer className='footer section__padding'>
+      <span className='footer__container'>
+        <div>
+          <h5>SHOP</h5>
+          <div>
+            {categories.categories.map((category) => {
+              return <p key={category._id}>{category.category_name}</p>;
+            })}
+          </div>
+        </div>
+        <div>
+          <h5>CORPORATE INFO</h5>
+          <div>
+            <p>Career at E.com</p>
+            <p>About E.com group</p>
+            <p>Press</p>
+          </div>
+        </div>
+        <div>
+          <h5>HELP</h5>
+          <div>
+            <p>Customer Service</p>
+            <p>My account</p>
+            <p>Contact</p>
+          </div>
+        </div>
+        <div>
+          <h5>JOIN NOW</h5>
+
+          <div>Become a member today</div>
+        </div>
+      </span>
+      <div className='footer__container footer-social'>
+        <p>facebook</p>
+        <p>twitter</p>
+        <p>instagram</p>
+        <p>youtube</p>
+        <p>pinterest</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
