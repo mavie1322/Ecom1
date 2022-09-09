24729b4deafdd656ee3c9321e601378177e9654d
BG <mavie1322@yahoo.fr>
2022-09-09 13:18:23 +0100

    make sure that user can go to the different categories,networking sites pages

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,49 +1,107 @@
-import React from "react";
-import { useAppSelector } from "../../hooks/hooks";
+import React, { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { BsTwitter, BsInstagram, BsYoutube, BsFacebook } from "react-icons/bs";
 import "./footer.css";
+import { Link, useNavigate } from "react-router-dom";
+import { categoriesActions } from "../../store/categories-slices";
+import SignIn from "../SignIn/SignIn";
 
 const Footer = () => {
+  const [openPopup, setOpenPopup] = useState<boolean>(false);
   const categories = useAppSelector((state) => state.categories);
+  const isLoggedIn = useAppSelector((state) => state.user.result);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleGoToAccount = () => {
+    if (isLoggedIn._id) {
+      navigate(`/users/${isLoggedIn._id}`);
+    } else {
+      setOpenPopup(!openPopup);
+    }
+  };
+
   return (
-    <footer className='footer section__padding'>
-      <span className='footer__container'>
+    <footer className='section__padding'>
+      <div className='footer__container'>
         <div>
           <h5>SHOP</h5>
-          <div>
+          <ul>
             {categories.categories.map((category) => {
-              return <p key={category._id}>{category.category_name}</p>;
+              return (
+                <Link
+                  to={"/"}
+                  key={category._id}
+                  className='link footer__container-category'
+                  onClick={() =>
+                    dispatch(
+                      categoriesActions.pickedCategory(category.category_name)
+                    )
+                  }>
+                  {category.category_name}
+                </Link>
+              );
             })}
-          </div>
+          </ul>
         </div>
         <div>
           <h5>CORPORATE INFO</h5>
-          <div>
-            <p>Career at E.com</p>
-            <p>About E.com group</p>
-            <p>Press</p>
-          </div>
+          <ul>
+            <li>Career at E.com</li>
+            <li>About E.com group</li>
+            <li>Press</li>
+          </ul>
         </div>
         <div>
           <h5>HELP</h5>
-          <div>
-            <p>Customer Service</p>
-            <p>My account</p>
-            <p>Contact</p>
-          </div>
+          <ul>
+            <li>Customer Service</li>
+            <li onClick={() => handleGoToAccount()}>My account</li>
+            <li>Contact</li>
+          </ul>
         </div>
-        <div>
-          <h5>JOIN NOW</h5>
-
-          <div>Become a member today</div>
-        </div>
-      </span>
-      <div className='footer__container footer-social'>
-        <p>facebook</p>
-        <p>twitter</p>
-        <p>instagram</p>
-        <p>youtube</p>
-        <p>pinterest</p>
       </div>
+      <ul className='footer-social link'>
+        <li>
+          <a
+            href='https://en-gb.facebook.com/'
+            target='_blank'
+            rel='noreferrer'
+            className='link'>
+            <BsFacebook size={18} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://twitter.com/login'
+            target='_blank'
+            rel='noreferrer'
+            className='link'>
+            <BsTwitter size={18} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.instagram.com/'
+            target='_blank'
+            rel='noreferrer'
+            className='link'>
+            <BsInstagram size={18} />
+          </a>
+        </li>
+        <li>
+          <a
+            href='https://www.youtube.com/'
+            target='_blank'
+            rel='noreferrer'
+            className='link'>
+            <BsYoutube size={18} />
+          </a>
+        </li>
+      </ul>
+      {openPopup && !isLoggedIn._id && (
+        <SignIn togglePopup={handleGoToAccount} />
+      )}
     </footer>
   );
 };
