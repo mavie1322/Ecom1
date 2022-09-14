b355b991a115a5cf2ae4da791b38ee0dc586f3fb
BG <mavie1322@yahoo.fr>
2022-09-14 23:47:12 +0100

    sort items list by price and divide the list by page. It add functionalities to buttons allowing user to browser items by page and disabled the same buttons when necessary

--- client/src/containers/Items.tsx
+++ client/src/containers/Items.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Article } from "../models";
 import { Link } from "react-router-dom";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
@@ -9,10 +9,31 @@ type Props = {
 
 const Items: React.FC<Props> = ({ itemsList }) => {
   const [pageNumber, setPageNumber] = React.useState<number>(1);
+  const [disablePreviousPage, setDisablePreviousPage] =
+    React.useState<boolean>(false);
+  const [disableNextPage, setDisableNextPage] = React.useState<boolean>(false);
   const [onSort, setOnSort] = React.useState<boolean>(false);
   const [sortBy, setSortBy] = React.useState<string>("");
-  console.log(itemsList.length);
+  const itemPerPage = 10;
+  /*
+  - make a copy of itemsList
+  - sort it if required
+  - list items by page
+  */
+  const itemsListCopy = [...itemsList];
+  if (sortBy === "ASC") {
+    itemsListCopy.sort((a, b) => a.price - b.price);
+  } else if (sortBy === "DESC") {
+    itemsListCopy.sort((a, b) => b.price - a.price);
+  }
+  let list = itemsListCopy.slice(
+    itemPerPage * pageNumber - itemPerPage,
+    itemPerPage * pageNumber
+  );
 
+  /*
+  -change color of the text in the dropdown
+  */
   const handleSortBy = (event: React.ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
     if (event.target.value === "ASC" || event.target.value === "DESC")
@@ -20,6 +41,34 @@ const Items: React.FC<Props> = ({ itemsList }) => {
     else setOnSort(false);
     setSortBy(event.target.value);
   };
+
+  const goToPreviousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (pageNumber < Math.ceil(itemsList.length / itemPerPage)) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
+
+  useEffect(() => {
+    //disabled or not buttons according to the page number
+    if (pageNumber === 1) setDisablePreviousPage(true);
+    else setDisablePreviousPage(false);
+
+    if (pageNumber === Math.ceil(itemsList.length / itemPerPage))
+      setDisableNextPage(true);
+    else setDisableNextPage(false);
+  }, [pageNumber, itemsList.length]);
+
+  useEffect(() => {
+    //in case you want to see items by categories and list is empty at that page number
+    if (list.length === 0) setPageNumber(1);
+  }, [list.length]);
+
   return (
     <>
       <div className='sortBy section__padding'>
@@ -32,7 +81,7 @@ const Items: React.FC<Props> = ({ itemsList }) => {
         </select>
       </div>
       <div className='home section__margin'>
-        {itemsList.map((item: Article) => {
+        {list.map((item: Article) => {
           const { _id, img_url, item_name, price } = item;
           return (
             <Link key={_id} to={`/products/${_id}`} className='link'>
@@ -46,11 +95,17 @@ const Items: React.FC<Props> = ({ itemsList }) => {
         })}
       </div>
       <div className='page section__padding'>
-        <button>
+        <button
+          type='button'
+          onClick={() => goToPreviousPage()}
+          disabled={disablePreviousPage}>
           <AiOutlineLeft />
         </button>
         <p>{pageNumber}</p>
-        <button>
+        <button
+          type='button'
+          onClick={() => goToNextPage()}
+          disabled={disableNextPage}>
           <AiOutlineRight />
         </button>
       </div>
